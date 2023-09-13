980eb378205dde6b8c08ea69d68601a18b0eb70f
boa901 <phillydooper@gmail.com>
2023-09-13 18:37:26 -0400

    Add wait_for_element method to Scraper
    Add an element to the Scraper class that makes the driver wait for an
    element to be present before continuing. This allows for the scraper to
    wait for elements on the page to be loaded before proceeding with
    following steps.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,33 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class Scraper:
+  by_aliases = {
+    'id': By.ID,
+    'name': By.NAME,
+    'xpath': By.XPATH,
+    'link_text': By.LINK_TEXT,
+    'partial_link_text': By.PARTIAL_LINK_TEXT,
+    'tag_name': By.TAG_NAME,
+    'class_name': By.CLASS_NAME,
+    'css_selector': By.CSS_SELECTOR
+  }
+
   def __init__(self, driver):
     print('Initializing scraper')
     self.driver = driver
 
   def get_url(self, url):
     self.driver.get(url)
+
+  def wait_for_element(self, by, identifier):
+    try:
+      element = WebDriverWait(self.driver, 10).until(
+        EC.presence_of_element_located((self.by_aliases[by], identifier))
+      )
+      return element
+    except TimeoutException:
+      print(f'Timeout error: Problem finding element with {by} of "{identifier}"')
+      self.driver.quit()
