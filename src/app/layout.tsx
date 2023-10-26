b66bf999ba5457ce83d4afed5c53c5f09b36adc5
Sean McClary <sdanielmc@gmail.com>
2023-10-25 20:58:03 -0400

    Initialize react front-end
    Add dependencies and create folder structure for a react front end using nextjs.

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Disc Golf Analysis',
+  description: 'Created by Noah Swartzentruber and Sean McClary',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
