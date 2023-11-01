4fc38c1ea21c66b1ff2222c356ae9a32bb23c9e8
boa901 <phillydooper@gmail.com>
2023-11-01 15:31:19 -0400

    Add get_pdga_event_data function in player_events
    Add a function to get event data from the pdga website in the
    player_events procedure.

--- selenium/procedures/player_events.py
+++ selenium/procedures/player_events.py
@@ -11,6 +11,38 @@ def get_event_data(row):
 
   return results
 
+def get_pdga_event_data(scraper):
+  results = dict()
+  results['event_name'] = scraper.get_element('xpath', '//div[@class="panel-pane pane-page-title"]/div/h1').get_attribute('innerText')
+  results['website'] = scraper.get_element('xpath', '//li[@class="tournament-website"]').get_attribute('innerText')[9::]
+
+  month_dict = {
+      "Jan": 1,
+      "Feb": 2,
+      "Mar": 3,
+      "Apr": 4,
+      "May": 5,
+      "Jun": 6,
+      "Jul": 7,
+      "Aug": 8,
+      "Sep": 9,
+      "Oct": 10,
+      "Nov": 11,
+      "Dec": 12,
+  }
+  date_range = scraper.get_element('xpath', '//li[@class="tournament-date"]').get_attribute('innerText')[6::]
+  start_date, end_date = date_range.split(' to ')
+  start_day, start_month = start_date.split('-')
+  end_day, end_month, year = end_date.split('-')
+  results['start_dt'] = f'{start_day:0>2}-{month_dict[start_month]:0>2}-{year}'
+  results['end_dt'] = f'{end_day:0>2}-{month_dict[end_month]:0>2}-{year}'
+
+  location = scraper.get_element('xpath', '//li[@class="tournament-location"]').get_attribute('innerText')[10::]
+  results['city'], results['state'], results['country'] = location.split(', ')
+
+  return results
+
+
 def scrape_player_events(scraper, pdga_no, profile):
   scraper.get_url(f'https://statmando.com/player/{profile}/profile')
   options = scraper.get_elements('xpath', '//option[starts-with(@value,"DGPT")]')
