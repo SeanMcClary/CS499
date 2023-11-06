f5f60e57a404861c39103e89028581b5dd72610a
Sean McClary <sdanielmc@gmail.com>
2023-11-06 18:27:27 -0500

    removed extraneous details for future page look

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,4 @@
 "use client"
-// export function player_table(){
-//   const p_list = [
-//     38008, "Richard", "Wysocki",
-//     27523, "Paul", "McBeth",
-//     37817, "Eagle", "McMahon",
-//     18824, "Nathan", "Sexton",
-//     27171, "Paul", "Ulibarri",
-//     68286, "Nathan", "Queen",
-//     17295, "James", "Conrad",
-//     33705, "Jeremy", "Koling",
-//     10671, "Chris", "Dickerson",
-//     24341, "Cale", "Leiviska"]
-//   p_list.map                                                               yeeet
-//     return(
-//       <tr>
-//         <td><a href="localhost:3000">name</a></td>
-//         <td>27523</td>
-//         <td>1049</td>
-//       </tr>
-//       )
-//   }
 
 import { useEffect, useState } from "react"
 
@@ -27,13 +6,11 @@ import { useEffect, useState } from "react"
 //Look into conditional rendering                                                                      LOOOK HERE
 
 
-
-export default function Home() {
-  const [intercept, setIntercept] = useState()
-  const [roundRating, setRoundRating] = useState()
+function PlayerList(){
+  const [players, setPlayers] = useState<any[]>([])
   useEffect(() => {
     async function fetchco() {
-      const response = await fetch('/api/model', {
+      const response = await fetch('/api/playerlist', {
           method: 'GET',
           headers: {
             'Accept': 'application/json',
@@ -42,35 +19,43 @@ export default function Home() {
         });
 
         const out =  await response.json()
-        console.log(out)
-        setIntercept(out['(Intercept)'])
-        setRoundRating(out.round_rating)
+        //console.log(out)
+        setPlayers(out)//this is where add out.map and you can display only certain things
     }
     fetchco()
-  }, ) 
+  },[] ) 
+
+  return(
+    <div>
+      <ul>{players?.map(player =>
+        <li>
+          {player.f_name} {player.l_name}, {player.pdga_no}
+        </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+
+export default function Home() {
   return (
     <>
       <title>Disc Golf Analysis Tool</title>
       <h1>
         Disc Golf Analysis Tool
       </h1>
-      <p>This is a tool designed to analyse tournament results of professional disc golfers and predict their future results</p>
-      <p>{intercept}</p>
-      <p>{roundRating}</p>
-      <table>
-
-        <tr>
-          <th>Name</th>
-          <th>PDGA number</th>
-          <th>Current Rating</th>
-        </tr>
-        <tr>
-         <td><a href="localhost:3000">name</a></td>
-         <td>27523</td>
-         <td>1049</td>
-       </tr>
-
-      </table>
-      </>
+      <p>This is a tool designed to analyse tournament results 
+        of professional disc golfers and predict their future 
+        results
+      </p>
+      <div>
+      <a href="localhost:3000"><button className="tab"> Home</button></a>
+      <button className="tab" onClick={PlayerList}> Player List</button>
+      </div>
+      
+      {PlayerList()}
+    </>
   )
 }
+
