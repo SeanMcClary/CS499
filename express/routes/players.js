49b3f90645b28f375409ef0a683af0a9505dfbe5
boa901 <phillydooper@gmail.com>
2023-11-15 15:45:39 -0500

    Add /players/:pdga_no/model express endpoint
    Add an endpoint at /players/:pdga_no/model to get a model based on a
    specific player for event results. This endpoint calls the player_model
    R script function getPredictedEventScore.

--- express/routes/players.js
+++ express/routes/players.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import R from 'r-integration';
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
@@ -24,5 +25,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:pdga_no', (req, res, next) => getPlayer(conn, req, res));
+
+router.get('/:pdga_no/model', (req, res, next) => {
+  const model = R.callMethod('./express/r_scripts/player_model.R', 'getPredictedEventScore', { x: parseInt(req.params.pdga_no) });
+  const result = {};
+  result[model[1]] = model[3];
+  result[model[2]] = model[4];
+  res.json(result);
+});
 
 export default router;
