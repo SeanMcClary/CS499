483588f80dc39d1107c18d4b8d3314223b7d88f1
boa901 <phillydooper@gmail.com>
2023-11-15 15:43:23 -0500

    Add player_model R script
    Add a player_model R script. Create getPredictedEventScore function that
    gives the coefficients for a regression model based on a single player
    across all events.

---
+++ express/r_scripts/player_model.R
@@ -0,0 +1,21 @@
+library(RMariaDB)
+
+readRenviron('.env')
+
+mysqlconnection <- dbConnect(
+  RMariaDB::MariaDB(),
+  dbname = Sys.getenv('MYSQL_DB_NAME'),
+  host = Sys.getenv('MYSQL_DB_HOST'),
+  port = strtoi(Sys.getenv('MYSQL_DB_PORT')),
+  user = Sys.getenv('MYSQL_DB_USERNAME'),
+  password = Sys.getenv('MYSQL_DB_PASSWORD'),
+)
+
+getPredictedEventScore <- function (x) {
+  query <- 'SELECT event_rating, stroke_ct FROM event_results WHERE pdga_no = ?'
+  result <- dbSendQuery(mysqlconnection, query)
+  dbBind(result, list(x))
+  data.frame <- dbFetch(result)
+  lmRound <- lm(stroke_ct~event_rating, data.frame)
+  return(lmRound$coefficients)
+}
