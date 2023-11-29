91f77f4d203a409f98fa991405f61ac730877f21
boa901 <phillydooper@gmail.com>
2023-11-29 15:37:35 -0500

    Add getPredictedEventScore function
    This function gets the coefficients of a regression model specific to
    the player's past results in previous years of the same event.

--- express/r_scripts/player_model.R
+++ express/r_scripts/player_model.R
@@ -19,3 +19,20 @@ getPredictedScore <- function (x) {
   lmRound <- lm(stroke_ct~event_rating, data.frame)
   return(lmRound$coefficients)
 }
+
+getPredictedEventScore <- function (x, y) {
+  query <- 'SELECT a.event_rating, a.stroke_ct
+    FROM event_results a
+    JOIN (
+      SELECT DISTINCT annual_event_id
+      FROM event_results
+      WHERE event_id = ?
+    ) b ON a.annual_event_id = b.annual_event_id
+    WHERE a.pdga_no = ?
+    AND event_rating > 0'
+  result <- dbSendQuery(mysqlconnection, query)
+  dbBind(result, list(y, x))
+  data.frame <- dbFetch(result)
+  lmRound <- lm(stroke_ct~event_rating, data.frame)
+  return(lmRound$coefficients)
+}
