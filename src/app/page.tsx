a5b12bd91db1fea697420fda9ac36550f90ec321
Sean McClary <sdanielmc@gmail.com>
2023-11-30 14:30:57 -0500

    search and formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,77 @@
 "use client"
-
 import { useEffect, useState } from "react"
-
-    
-//Look into conditional rendering                                                                      LOOOK HERE
-
+import "./styles.css";
 
 function PlayerList(){
-  const [players, setPlayers] = useState<any[]>([])
-  useEffect(() => {
-    async function fetchco() {
-      const response = await fetch('/api/playerlist', {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-        });
+  const [players, setPlayers] = useState<any[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
-        const out =  await response.json()
-        //console.log(out)
-        setPlayers(out)//this is where add out.map and you can display only certain things
+  useEffect(() => {
+    async function fetchPlayers() {
+      const response = await fetch('/api/playerlist', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const playerData = await response.json();
+      setPlayers(playerData);
     }
-    fetchco()
-  },[] ) 
+
+    fetchPlayers();
+  }, []);
+
+  const filteredPlayers = players.filter(player =>
+    `${player.f_name} ${player.l_name}`.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return(
     <div>
-      <ul>{players?.map(player =>
-        <li>
-          <a href={"/player-data/" + player.pdga_no} target="_blank">{player.f_name} {player.l_name}</a>
-        </li>
+      <input
+        type="text"
+        placeholder="Search by player name"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <ul>
+        {filteredPlayers.map(player =>
+          <li key={player.pdga_no}>
+            <a href={"/player-data/" + player.pdga_no} target="_blank">
+              {player.f_name} {player.l_name}
+            </a>
+          </li>
         )}
       </ul>
     </div>
-  )
+  );
 }
-
 
 export default function Home() {
   return (
     <>
-      <title>Disc Golf Analysis Tool</title>
-      <h1>
-        Disc Golf Analysis Tool
-      </h1>
-      <p>This is a tool designed to analyse tournament results 
-        of professional disc golfers and predict their future 
-        results
+    <div className="header">
+      <title>AnalyDisc</title>
+      <h1>AnalyDisc</h1>
+      <p>
+        This is a tool designed to analyze <br/>
+        tournament results of professional disc <br/>
+        golfers and predict their future results.
       </p>
-      <div>
-      <a href="localhost:3000"><button className="tab"> Home</button></a>
-      <button className="tab" onClick={PlayerList}> Player List</button>
+    </div>
+
+    <div className="container">
+      <div className="player-list">
+        <PlayerList />
+      </div>
+      <div className="main-content">
+        <p>
+          content
+        </p>
       </div>
       
-      {PlayerList()}
+    </div>
     </>
-  )
+  );
 }
-
