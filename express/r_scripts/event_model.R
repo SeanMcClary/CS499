559d877719f5c7c35d877d445eb204d333e24bbc
boa901 <phillydooper@gmail.com>
2023-12-05 21:24:26 -0500

    Create the event_model R script
    Create the event_modal R script. This gives a model for a specific
    annual event based on all players' participation.

---
+++ express/r_scripts/event_model.R
@@ -0,0 +1,26 @@
+library(RMariaDB)
+
+readRenviron('.env')
+
+mysqlconnection <- dbConnect(
+  RMariaDB::MariaDB(),
+  dbname = Sys.getenv('MYSQL_DB_NAME'),
+  host = Sys.getenv('MYSQL_DB_HOST'),
+  port = strtoi(Sys.getenv('MYSQL_DB_PORT')),
+  user = Sys.getenv('MYSQL_DB_USERNAME'),
+  password = Sys.getenv('MYSQL_DB_PASSWORD'),
+)
+
+getPredictedScore <- function (x) {
+  query <- 'SELECT event_rating, stroke_ct
+    FROM event_results
+    WHERE annual_event_id = ?
+    AND event_rating > 0'
+  result <- dbSendQuery(mysqlconnection, query)
+  dbBind(result, list(x))
+  data.frame <- dbFetch(result)
+  dbClearResult(result)
+  dbDisconnect(mysqlconnection)
+  lmRound <- lm(stroke_ct~event_rating, data.frame)
+  return(lmRound$coefficients)
+}
