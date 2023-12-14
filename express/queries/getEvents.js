395de4457a1665b136ec6ff0f1d27c700f7a4398
boa901 <phillydooper@gmail.com>
2023-12-13 22:29:06 -0500

    Add id to events query
    Add id to events query even when a pdga number is specified.

--- express/queries/getEvents.js
+++ express/queries/getEvents.js
@@ -2,7 +2,7 @@ export default function getEvents(conn, req, res) {
   conn.connect((e) => {
     if (req.query.pdga_no) {
       const pdga_no = parseInt(req.query.pdga_no);
-      const query = `select distinct b.event_name, b.city, b.state, b.country from event_results a
+      const query = `select distinct b.event_name, b.city, b.state, b.country, b.id from event_results a
         join events b on a.annual_event_id = b.id
         where a.pdga_no = ?`
       conn.query(query, [pdga_no], (e, result) => {
